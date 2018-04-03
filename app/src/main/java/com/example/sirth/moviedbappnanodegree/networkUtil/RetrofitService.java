a334645f245d6a9efbafabdffb3267af1869a1ac
Amunratis <sirthesurfer@gmail.com>
2018-04-03 18:58:13 +0200

    Stage 2 completed

--- app/src/main/java/com/example/sirth/moviedbappnanodegree/networkUtil/RetrofitService.java
+++ app/src/main/java/com/example/sirth/moviedbappnanodegree/networkUtil/RetrofitService.java
@@ -1,6 +1,8 @@
 package com.example.sirth.moviedbappnanodegree.networkUtil;
 
 import com.example.sirth.moviedbappnanodegree.dataModel.MoviePages;
+import com.example.sirth.moviedbappnanodegree.dataModel.MovieReview;
+import com.example.sirth.moviedbappnanodegree.dataModel.MovieTrailer;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -17,9 +19,10 @@ public interface RetrofitService  {
     Call<MoviePages> getPopularMovies(@Query("api_key")String apikey);
     @GET("movie/top_rated")
     Call<MoviePages> getTopRated(@Query("api_key")String apikey);
-    @GET("/movie/{id}/videos ")
-    Call<MoviePages> getTrailers(@Path("id") int id, @Query("api_key")String apikey);
-    @GET("/movie/{id}/reviews")
-    Call<MoviePages> getReviews(@Path("id") int id, @Query("api_key")String apikey);
+
+    @GET("movie/{id}/videos")
+    Call<MovieTrailer> getTrailers(@Path("id") int id, @Query("api_key")String apikey);
+    @GET("movie/{id}/reviews")
+    Call<MovieReview> getReviews(@Path("id") int id, @Query("api_key")String apikey);
 
 }
